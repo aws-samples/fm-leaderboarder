2aabc10a6373773b63a17ffde9288cda8809694b
Gili Nachum <gili@amazon.com>
2024-03-19 18:14:05 +0000

    Add tooltips and Legend to main html report

--- utils/dashboard_creators/dashboard_template.py
+++ utils/dashboard_creators/dashboard_template.py
@@ -1,9 +1,31 @@
 import html
 
+def get_optional_tooltip_html(name : str):
+    tips_by_metric = { 
+        "meteor" : "METEOR is a metric for text similarity between the machine-produced summary and human-produced reference summaries.",
+        "rouge" : "The ROUGE metric measures text similarity by computing overlapping n-grams between a machine-generated text and one or more reference human-written texts.",
+        "bertscore" : "The BERTScore is a text similarity metric that leverages BERT's contextual embeddings to compute token similarities between the candidate and reference texts.",
+        "bartscore" : "",
+    }
+    if name.lower().startswith("metric:"):
+        metric_name = name.lower().split(' ')[-1]
+        if metric_name in tips_by_metric:  
+            tip = tips_by_metric[metric_name]             
+            tooltip_html ='''
+                <span class="tooltip">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><circle cx="12" cy="12" r="10"></circle><line x1="12" y1="16" x2="12" y2="12"></line><line x1="12" y1="8" x2="12.01" y2="8"></line></svg>
+                    <span class="tooltiptext">{}</span>
+                </span>
+            '''.format(tip)
+            return tooltip_html
+    return ""
+            
+
 def generate_dashboard_string(title = 'page title', pre_table_html = "", column_names = [], rows = []):
     columns_html = ""
     for name in column_names:
-        columns_html += f"<th>{html.escape(str(name))}</th>\n"
+        tooltip_html = get_optional_tooltip_html(str(name))
+        columns_html += f"<th>{html.escape(str(name))}" + f"{tooltip_html}</th>\n"
 
     table_data_html = ""
     for row in rows:
@@ -24,6 +46,7 @@ def generate_dashboard_string(title = 'page title', pre_table_html = "", column_
 <link rel="stylesheet" href="https://cdn.datatables.net/2.0.1/css/dataTables.dataTables.css" />
 <head>
 <title>{title}</title>
+<link rel="stylesheet" href="styles.css">
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
 <script src="https://cdn.datatables.net/2.0.1/js/dataTables.js"></script>
 <script>
@@ -73,13 +96,13 @@ def generate_dashboard_string(title = 'page title', pre_table_html = "", column_
 </table>
 </body>
 </html>
-    '''.format(**args)
+'''.format(**args)
 
 # testcases
 def test_generate_dashboard_string():
     print(generate_dashboard_string(title = 'mytitle', column_names = ["a", "b"], rows = [[1, 2], [3, 4]]))
     print(generate_dashboard_string(title = 'mytitle', pre_table_html= "<div>1</div>", column_names = ["a", "b"], rows = [[1, 2], [3, 4]]))
     print(generate_dashboard_string(column_names = ["a", "b"], rows = [['<a href="..">link</a>', 2], [3, 4]]))
-
+    print(generate_dashboard_string(column_names = ["Metric: meteor", "b"], rows = [['<a href="..">link</a>', 2], [3, 4]]))
 
 #test_generate_dashboard_string()
--- utils/dashboard_creators/main_html_creator.py
+++ utils/dashboard_creators/main_html_creator.py
@@ -18,8 +18,20 @@ def create_main_html(result_folder, models_scores):
     <li><a href="html_files/output_comparison.html">Side-by-side model responses</a></li>
 </ul>
 
-<h2>Leaderboard</h2>
-Note you can sort columns and search by a keyword to filter
+<h2>
+    Leaderboard
+    <span class="tooltip">
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><circle cx="12" cy="12" r="10"></circle><line x1="12" y1="16" x2="12" y2="12"></line><line x1="12" y1="8" x2="12.01" y2="8"></line></svg>
+        <span class="tooltiptext">You can sort by columns and search by a keyword to filter</span>
+      </span>
+</h2>
+
+<div id="legend">
+  Legend: 
+  <span style="background-color: darkgreen; color: white; padding: 2px 5px;">1st Best Result</span>
+  <span style="background-color: green; color: white; padding: 2px 5px;">2nd Best Result</span>
+  <span style="background-color: lightgreen; padding: 2px 5px;">3rd Best Result</span>
+</div>
     """
 
   # generate headers name
@@ -42,5 +54,12 @@ Note you can sort columns and search by a keyword to filter
     with open(index_filename, "w", encoding='utf-8-sig') as file:
         from .dashboard_template import generate_dashboard_string
         file.write(generate_dashboard_string(title = title, pre_table_html = pre_table_html, column_names = headers, rows = rows))
+
+    # CSS
+    # copy CSS file from ./static/styles.css to the result folder
+    # get current python file folder
+    import os
+    import shutil
+    shutil.copyfile(f'{os.path.dirname(os.path.abspath(__file__))}/static/styles.css', f'{result_folder}/styles.css')
 
     return index_filename
